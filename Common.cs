1e1200e1e4f5f87c45166cee5432274e39ab07b0
GNANASOUNDARIYA S <104256780+Gnanasoundariya478@users.noreply.github.com>
2023-10-30 00:11:58 +0530

    Add files via upload

---
+++ Common.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace Online_Money_Transaction
+{
+    public class Common
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["MoneyTransactionDBConnectionString"].ConnectionString;
+        }
+    }
+}
